19df8ecb3d9bcdf94bbf7ffeff26d0a76dc29ce7
npex42 <npex42@gmail.com>
2021-06-21 13:56:55 +0100

    Bumped Version To 0.7.0

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -4,13 +4,30 @@ pub mod time;
 pub mod input;
 
 pub struct Arguments {
-    parent : &'static str
+    parent : &'static str,
+    args   : Option<&'static [&'static str]> 
 }
 
 impl Arguments {
     pub fn empty() -> Self {
         Self {
-            parent : "none"
+            parent : "none",
+            args : None
         }
+    }
+
+    pub fn new(parent : &'static str, args : &'static str) -> Self {
+        Self {
+            parent,
+            args : None
+        }
+    }
+
+    pub fn parent(&self) -> &'static str {
+        self.parent
+    }
+
+    pub fn args(&self) -> Option<&'static [&'static str]> {
+        self.args
     }
 }
